6364d0d3c60764fc5abc19ea277c3dfbc3da3eb4
Daniel Hammer <braineniac@protonmail.com>
2020-02-07 21:14:41 +0100

    Change setup.py layout

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
-setup(
-    version="0.0.1",
-    scripts=['bin/'],
+setup_args = generate_distutils_setup(
     packages=['nodes'],
-    package_dir={'': 'src'}
-)
+    package_dir={'': 'src'})
+
+setup(**setup_args)
